8bf268ab0f8973a4c0edf3ace4664e99a9487308
tschefke <tschef4@lsu.edu>
2024-11-02 08:09:40 +0900

    Update cards & run.sh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NEUTvectReader
+
+# About 
+This is the base for investigations into TKI variables in 3-body interactions. 
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 source /home/t2k/tschefke/work/tki_studies/setup_env.sh
+WORKDIR="/gpfs/group/t2k/beam/work/tschefke/tki_studies/NEUTvectReader"
+echo $PWD
+neutroot2 ./neut_cards/neut_rs_13a_CH_numubar_noFSI.card ./output/rhc_noFSI.root
 
-neutroot2 neut_cards/neut_rs_13a_CH_numubar.card output/rhc_13a_10c6events.root
+mv $WORKDIR/output/rhc_noFSI.root output/rhc_13a_1e6_rs_noFSI.root
