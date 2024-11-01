33775f76b8753ab84a3a3bdf3a5be535374fd3de
Tristan Schefke <tschefke3@gmail.com>
2024-11-02 01:35:28 +0900

    Initial commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+source /home/tschefke/setup_neut564.sh
+source /usr/local/sklib_gcc8/root_v6.22.06_python3.8/bin/thisroot.sh
+source /disk03/usr8/tschefke/soft/neut/build/Linux/setup.sh
+
+neutroot2 neut_rs_13a_CH.card output/rhc_13a_10c6events.root
